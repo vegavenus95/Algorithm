db8db4543e9c993572e4f0ef66391754a12a7ba4
HG <vegavenus0920@gmail.com>
2021-08-29 11:30:00 +0900

    [2021.08.29] algo study

---
+++ BOJ/src/algo_210829/boj_1000.java
@@ -0,0 +1,18 @@
+package algo_210829;
+
+import java.util.Scanner;
+
+public class boj_1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int result = A + B;
+		System.out.println(result);
+		
+	}
+
+}
